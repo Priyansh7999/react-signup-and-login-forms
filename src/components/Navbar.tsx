823abd09e587de089d7a8a68e8577aa30463e6fc
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-27 15:26:04 +0530

    feat: add navigation links to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <nav className="w-full p-4 sm:px-20 sm:py-6 flex items-center justify-between">
+        <nav className="w-full p-4 sm:px-20 sm:py-6 flex items-center justify-between shadow-2xs">
             <h1 className="text-2xl font-bold text-sky-800">REACT</h1>
+
+            <ul className="flex items-center gap-6">
+                <li>
+                    <Link
+                        to="/tickets/create"
+                        className="text-lg font-medium hover:text-sky-800 transition duration-300 ease-in-out"
+                    >
+                        Create Ticket
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/tickets"
+                        className="text-lg font-medium hover:text-sky-800 transition duration-300 ease-in-out"
+                    >
+                        Tickets
+                    </Link>
+                </li>
+            </ul>
         </nav>
     )
 }
