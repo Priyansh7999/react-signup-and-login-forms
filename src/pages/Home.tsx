08952d7fadfb43c2cd71f69695a1f12d4bfbd035
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-27 15:25:42 +0530

    refactor: remove manual auth check from Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import { getAuthToken } from "../utils/authToken";
-import { useEffect } from "react";
-import CreateTicket from "../components/CreateTicket";
 
 const Home = () => {
-    const token = getAuthToken();
-    const navigate = useNavigate();
-    
-    useEffect(() => {
-        if (!token) {
-            navigate("/login");
-        }
-    }, [token]);
-
     return (
         <>
             <Navbar />
-
-            <CreateTicket />
         </>
     )
 }
