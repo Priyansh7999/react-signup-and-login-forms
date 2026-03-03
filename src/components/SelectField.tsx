988105e5a4d989d03bcc04d241ad1d3cba96dbcb
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-03 19:23:35 +0530

    refactor: make SelectField component generic and reusable

--- src/components/SelectField.tsx
+++ src/components/SelectField.tsx
@@ -1,43 +1,51 @@
 import { useField } from "formik";
-import type { SupportAgentType } from "../types/user";
 import type React from "react";
 
-type SelectFieldProps = {
-    name: string;
-    label: string;
-    options: string[] | SupportAgentType[];
-}
+type SelectFieldProps<T> = {
+  name: string;
+  label: string;
+  options: T[];
+  getOptionLabel: (option: T) => string;
+  getOptionValue: (option: T) => string;
+};
 
-const SelectField = ({ label, options, ...props }: SelectFieldProps) : React.JSX.Element=> {
-    const [field, meta] = useField(props);
+function SelectField<T>({label, options, getOptionLabel, getOptionValue, ...props}: SelectFieldProps<T>): React.JSX.Element {
+    
+  const [field, meta] = useField(props.name);
 
-    return (
-        <div className="flex flex-col gap-1">
-            <label htmlFor={props.name} className="text-sm font-medium text-neutral-700">
-                {label}
-            </label>
+  return (
+    <div className="flex flex-col gap-1">
+      <label
+        htmlFor={props.name}
+        className="text-sm font-medium text-neutral-700"
+      >
+        {label}
+      </label>
 
-            <select
-                id={props.name}
-                {...field}
-                {...props}
-                className="p-2.5 rounded-lg border text-sm outline-none transition-all duration-200"
-            >
-                <option value="">Select {label}</option>
-                {options.map(option =>
-                    typeof option === "string" ? (
-                        <option key={option} value={option}>{option}</option>
-                    ) : (
-                        <option key={option.id} value={option.id}>{option.name}</option>
-                    )
-                )}
-            </select>
+      <select
+        id={props.name}
+        {...field}
+        className="p-2.5 rounded-lg border text-sm outline-none transition-all duration-200"
+      >
+        <option value="">Select {label}</option>
 
-            {meta.error && (
-                <div className="text-xs text-red-500 mt-0.5">{meta.error}</div>
-            )}
+        {options.map((option, index) => (
+          <option
+            key={index}
+            value={getOptionValue(option)}
+          >
+            {getOptionLabel(option)}
+          </option>
+        ))}
+      </select>
+
+      {meta.error && (
+        <div className="text-xs text-red-500 mt-0.5">
+          {meta.error}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default SelectField;
