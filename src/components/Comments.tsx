281f98e40961270db9333e34c8b6d703612c076f
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-03 17:17:33 +0530

    refactor:convert Comments component to presentational component

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,24 +1,15 @@
-import { useEffect, useState } from "react";
 import type { CommentType } from "../types/comment";
-import { getCommentsByTicketId } from "../api/ticket.api";
 
-const Comments = ({ id }: { id: string | undefined }) => {
-    const [comments, setComments] = useState<(CommentType | null)[]>();
-
-    useEffect(() => {
-        const fetchTicket = async () => {
-            const data = await getCommentsByTicketId(id);
-            setComments(data.data);
-        };
-
-        fetchTicket();
-    }, [id]);
+type Props ={
+    commentsList : CommentType[]
+}
+const Comments = ({commentsList}:Props) => {
 
     return (
         <div className="border-t border-neutral-200 pt-4 space-y-4">
             <h1 className="font-semibold">Comments</h1>
 
-            {comments?.map((comment) => (
+            {commentsList?.map((comment) => (
                 <div key={comment?.id}>
                     <div className="flex items-center gap-4 text-sm text-neutral-400">
                         <h5>{comment?.createdAt ? new Date(comment.createdAt).toLocaleDateString() : "-"}</h5>
@@ -29,7 +20,7 @@ const Comments = ({ id }: { id: string | undefined }) => {
                 </div>
             ))}
 
-            {comments?.length === 0 &&
+            {commentsList?.length === 0 &&
                 <p className="text-neutral-400">No Comments</p>
             }
         </div>
