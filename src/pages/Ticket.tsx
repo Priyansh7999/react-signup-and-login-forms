01f2a79e64fbf52fdb735b6d3bd53cdd63ea3409
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-03 17:06:33 +0530

    feat:integrate AddComment component into Ticket page

--- src/pages/Ticket.tsx
+++ src/pages/Ticket.tsx
@@ -1,14 +1,15 @@
 import { useParams } from 'react-router-dom';
-import { getTicketById } from '../api/ticket.api';
+import { addComment, getTicketById } from '../api/ticket.api';
 import { useEffect, useState } from 'react';
 import type { TicketType } from '../types/ticket';
-import useIsAllowed from '../hooks/useIsAllowed';
 import TicketCard from '../components/TicketCard';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import AddComment from '../components/AddComment';
 
 export default function Ticket() {
     const { id } = useParams();
     const [ticket, setTicket] = useState<TicketType | null>(null);
-    const isAllowed = useIsAllowed();
 
     useEffect(() => {
         const fetchTicket = async () => {
@@ -18,10 +19,24 @@ export default function Ticket() {
 
         fetchTicket();
     }, [id]);
+
+    const handleAddComment=async(values:{body:string}):Promise<void>=>{
+      try {
+            const data = await addComment(id, values.body);
+            if (data.success) {
+                toast.success(data.message);
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                toast.error(error.response?.data?.message);
+            }
+        }
+    }
     
   return (
     <div>
       <TicketCard ticketDetails={ticket} />
+      <AddComment handleAddComment={handleAddComment} />
     </div>
   )
 }
