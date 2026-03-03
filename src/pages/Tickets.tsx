634fa7fc5bc2470fb139432e0d00abb2f3cd2aaa
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-03 22:43:36 +0530

    feat: use reusable Table component in Tickets to display all tickets

--- src/pages/Tickets.tsx
+++ src/pages/Tickets.tsx
@@ -3,91 +3,54 @@ import { getAllTickets } from "../api/ticket.api";
 import type { TicketType } from "../types/ticket";
 import useIsAllowed from "../hooks/useIsAllowed";
 import TicketActions from "../components/TicketActions";
+import Table from "../components/Table";
 
 const Tickets = () => {
-    const [tickets, setTickets] = useState<(TicketType | null)[]>();
+    const [tickets, setTickets] = useState<TicketType[]>([]);
     const isAllowed = useIsAllowed();
 
     useEffect(() => {
-        const fetchTickets = async () : Promise<void> => {
+        const fetchTickets = async () => {
             const data = await getAllTickets();
             setTickets(data.data);
         };
-
         fetchTickets();
     }, []);
+
+    const columns = [
+        {
+            header: "Title",
+            render: (ticket: TicketType) => ticket.title,
+        },
+        {
+            header: "Description",
+            render: (ticket: TicketType) => ticket.description,
+        },
+        {
+            header: "Status",
+            render: (ticket: TicketType) => ticket.status,
+        },
+        {
+            header: isAllowed("VIEW_OWN_TICKETS") ? "Support Agent" : "Priority",
+            render: (ticket: TicketType) => isAllowed("VIEW_OWN_TICKETS") ? ticket.agentName : ticket.priority,
+        },
+        {
+            header: "Created At",
+            render: (ticket: TicketType) => new Date(ticket?.createdAt!).toLocaleDateString()
+        },
+        {
+            header: "Actions",
+            render: (ticket: TicketType) => (<TicketActions ticketId={ticket.id} status={ticket.status} />
+            ),
+        },
+    ];
 
     return (
         <div className="w-full flex flex-col items-center justify-center gap-4 p-4 md:p-20">
             <h1 className="text-xl sm:text-2xl font-extrabold">Tickets</h1>
-
-            <div className="w-full shadow-xs rounded-2xl border border-neutral-200 bg-white overflow-x-auto">
-                <table className="w-full text-sm text-left">
-                    <thead className="text-sm text-body border-b border-neutral-200">
-                        <tr>
-                            <th className="px-3 md:px-6 py-3 font-semibold">
-                                Title
-                            </th>
-                            <th className="px-3 md:px-6 py-3 font-semibold">
-                                Description
-                            </th>
-                            <th className="px-3 md:px-6 py-3 font-semibold">
-                                Status
-                            </th>
-                            <th className="px-3 md:px-6 py-3 font-semibold text-nowrap">
-                                {isAllowed("VIEW_OWN_TICKETS") && "Support Agent"}
-                                {isAllowed("VIEW_ASSIGNED_TICKETS") && "Priority"}
-                            </th>
-                            <th className="px-3 md:px-6 py-3 font-semibold">
-                                Create At
-                            </th>
-                            <th className="px-3 md:px-6 py-3 font-semibold">
-                                Actions
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tickets?.map((ticket) => (
-                            <tr key={ticket?.id} className="border-b last:border-b-0 border-neutral-200">
-                                <td className="px-3 md:px-6 py-4 font-medium whitespace-nowrap">
-                                    {ticket?.title}
-                                </td>
-                                <td className="px-3 md:px-6 py-4 max-w-md">
-                                    <p className="truncate text-nowrap">
-                                        {ticket?.description}
-                                    </p>
-                                </td>
-                                <td className="px-3 md:px-6 py-4">
-                                    {ticket?.status}
-                                </td>
-                                <td className="px-3 md:px-6 py-4 text-center text-nowrap">
-                                    {isAllowed("VIEW_OWN_TICKETS") && ticket?.agentName}
-                                    {isAllowed("VIEW_ASSIGNED_TICKETS") && ticket?.priority}
-                                </td>
-                                <td className="px-3 md:px-6 py-4">
-                                    {ticket?.createdAt ? new Date(ticket.createdAt).toLocaleDateString() : "-"}
-                                </td>
-                                <td className="px-3 md:px-6 py-4 space-x-1">
-                                    <TicketActions
-                                        ticketId={ticket?.id}
-                                        status={ticket?.status}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
-
-                        {tickets?.length === 0 &&
-                            <tr>
-                                <td colSpan={6} className="py-10 text-center text-neutral-400">
-                                    No tickets found
-                                </td>
-                            </tr>
-                        }
-                    </tbody>
-                </table>
-            </div>
+            <Table data={tickets} columns={columns}/>
         </div>
-    )
-}
+    );
+};
 
 export default Tickets;
